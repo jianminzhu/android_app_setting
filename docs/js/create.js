0d857bfaa335cb52426f24e48cac8989ac20217b
zjm <zhujianmin2009@163.com>
2020-01-08 19:13:21 +0800

    index

--- docs/js/create.js
+++ docs/js/create.js
@@ -1,6 +1,5 @@
 eSrc = document.createElement("script");
-eSrc.type="type/script"
-eSrc.src="https://jianminzhu.github.io/android_app_setting/js/addjs.js"
+eSrc.type = 'text/javascript';
+eSrc.src = "https://jianminzhu.github.io/android_app_setting/js/addjs.js"
 document.getElementsByTagName("HEAD")[0].appendChild(eSrc);
-
 
