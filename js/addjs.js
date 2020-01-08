feb12e41cc1272894fd90db4b9fe37c1ccbc4856
zjm <zhujianmin2009@163.com>
2020-01-08 18:39:47 +0800

    add alt+s

---
+++ js/addjs.js
@@ -0,0 +1,19 @@
+function addFrameScript(iframe,src){
+    let contentWindow = iframe.contentWindow;
+    if(contentWindow.isLoad!=true){
+          var frmDoc=contentWindow.document;
+            var ma = frmDoc.createElement('script');
+            ma.type = 'text/javascript';
+            ma.src = src
+            frmDoc.getElementsByTagName("HEAD")[0].appendChild(ma);
+    }else{
+        console.log("loaded")
+    }
+}
+setInterval(function() {
+   var iframes= document.getElementsByTagName("iframe")
+   for(var i=0;i<iframes.length;i++){
+        iframe=iframes[i];
+        addFrameScript(iframe,"hotkeys.ext.min.js")
+   }
+},6000);
